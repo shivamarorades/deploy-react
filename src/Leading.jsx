bef24c7df0743ef090a5870adb360c5676353feb
Shivam Arora <arora1897@gmail.com>
2020-11-17 12:32:46 +0530

    first commit

---
+++ src/Leading.jsx
@@ -0,0 +1,149 @@
+import React from 'react';
+import logo1 from './medical-logo-png-32-removebg.png';
+import logo2 from './hair.jpg';
+
+console.log(logo1);
+console.log(logo2);
+
+let Leading=()=>{
+  return(
+
+<div>
+  
+  <nav>
+
+    <img src={logo1} width="50" height="60" alt="bonelogo"/>
+    <li className="logo">skin specialist  </li>
+   
+    <ul>
+      <li><a href="#background">home</a></li>
+      <li><a href="#section1">services</a></li>
+      <li><a href="#contacts">contact</a></li>
+      <li><a href="#lastsec">about</a></li>
+      <li><a href="#blogger">info</a></li>
+    </ul>
+  </nav>
+  
+<section Id="background">
+<h1 className="h11"><span className="h111">w</span>elcome to </h1>
+<h1 className="h12"><span className="h112">s</span>ai clinic</h1>
+
+<h1 className="h16"><marquee>note down mobile number : <span Id="spanation"> 98714-38960</span> MBBS, MD</marquee></h1>
+<h1 className="h14"><marquee>skin, hair, nail and gupt rog</marquee><marquee>family physician</marquee></h1>
+</section>
+<div>
+  <section Id="blogger">
+  <h1 className="header2">Business Information</h1><br/>
+  <p></p>
+  <h1 className="header2">Dr. R K Garg (Sai Skin Centre) in Vikaspuri, Delhi</h1>
+  <p>Dr. R K Garg (Sai Skin Centre) in Delhi. Dermatologists with Address, Contact Number, Photos, Maps. View Dr. R K Garg (Sai Skin Centre), Delhi on Justdial.</p>
+  <br/>
+  <p></p>
+  <h2 className="header2">Location and Overview </h2>
+  <p></p>
+  <p>A qualified medical practitioner, Dr. R K Garg (Sai Skin Centre) in Vikaspuri, Delhi is one among the celebrated Dermatologists, having practiced the medical specialization for many years. This medical practitioner's clinic was established a while ago and since then, it has drawn scores of patients not only from in and around the neighbourhood but also from the neighbouring areas as well. This medical professional is proficient in identifying, diagnosing and treating the various health issues and problems related to the medical field. This doctor has the requisite knowledge and the expertise not just to address a diverse set of health ailments and conditions but also to prevent them. As a trained medical professional, this doctor is also familiar with the latest advancements in the related field of medicine. The clinic of this doctor is at DG- 3b, 1st Floor , Sai Skin Centre,Vikaspuri-110018. The commute to this clinic is a rather convenient one as various modes of conveyance are easily available. In order to get more information and to schedule an appointment with this doctor, citizens can reach out on the following contact number +(91)-11-41537863. This listing is also listed in Dermatologists.  </p>
+  <p></p>
+  <br/>
+  <p className="header2"><b>Services offered by Dr. R K Garg (Sai Skin Centre)</b></p>
+  <p></p>
+  <p></p>
+  <p>Dr. R K Garg (Sai Skin Centre) in Vikaspuri has a well-equipped and well-maintained clinic. It is sectioned into a waiting area for patients, where they can wait for their turn. This clinic has a consulting room, where this practitioner attends to patients between the consulting timings. Usually, on the first visit, this practitioner thoroughly understands and documents the patient's medical history and discusses the various health issues they are currently facing. Following this, the doctor may conduct a simple examination to check and confirm the symptoms of the ailment. Based on this examination, this physician prescribes a suitable course of action, be it medication or further diagnostic tests. This clinic is open to patients between 12:00-18:30 - 16:00-21:30. Make a payment with ease using any of the following modes of payment, including Cash. </p>
+  <br/>
+  <p>Please scroll to the top for the address and contact details of Dr. R K Garg (Sai Skin Centre) at Vikaspuri, Delhi. </p>
+  
+  <br/></section>
+</div>
+
+  
+  <div className="div2">
+  
+    <section Id="section1">our <br/>facilities: <br/></section>
+ 
+    <h4 className="ol1">hair transplant</h4>
+    <h4  className="ol1">robotic fue tech</h4>
+    <h4  className="ol1">prp for baldness</h4>
+    <h4  className="ol1">skin rejuvenation lasers</h4>
+    <h4  className="ol1">birth mark</h4>
+    <h4  className="ol1">acne scars</h4>
+ 
+    </div>
+
+    <div className="div3">
+    <section Id="section2">microderma<br/>abrasion <br/></section>
+    <span></span>
+    <h2 className="ol2">botox</h2>
+    <h2 className="ol2">Fillers</h2>
+    <h2 className="ol2">misotherapy</h2>
+    <h2 className="ol2">leucorrhea</h2>
+    
+   </div>
+   <div className="div4">
+    <section Id="section3">specialist <br/>in : <br/></section>
+ 
+    <h3 className="ol3">fever</h3>
+    <h3 className="ol3">hypertension `(bp)`</h3>
+    <h3 className="ol3">diabetes</h3>
+    <h3 className="ol3">Female disease problems</h3>
+ 
+    </div>
+    
+ <h1 className="h192"><marquee className="">family physician also available on appointment</marquee></h1>   
+ 
+<div Id="contacts">
+  <section className="articles">
+    <h1 className="childartic">Contact us</h1>
+   <div className="parentartic">
+     <section className="val1"><h1>Dr. r.k. garg<br/><span Id="spanation">98714-38960</span></h1><p>ex-consultant:- Kaya skin clinic</p>
+     <p>sundar lal jain hospital</p>
+     <p>consultant : gupta nursing home <br/>uttam-nagar</p>
+     </section>
+     <section className="val2"><img className="logo21" src={logo2} alt="hairstyle"/><h1>member : ima, iadvl</h1></section>
+   </div>
+  </section>
+</div>
+<h1 className="h192"><marquee className="">children and adult vaccination available</marquee></h1>
+ <div className="last"> 
+  <footer Id="lastsec">
+    <h1 className="whitte">about</h1>
+<table>
+<tr className="th1">
+  <th>clinic 1 : </th>
+  <td>dg-II/3b</td> <br/><td>vikaspuri</td> <br/><td>near agarwaal sweets</td> <br/><td>new delhi - 110018</td>
+  
+</tr>
+<tr className="th2">
+  <th >timing : </th>
+  <td>12:00pm to 4.00 pm </td><br/><td> 6-30 pm to 9-30 pm `(mon - fri)`</td>
+  <br/><td>11.00 am to 6.00 pm </td><br/><td> 7-30pm to 10-00pm `(sat)`</td>
+  <td><h3>sunday with appointment</h3></td>
+  </tr>
+</table>
+<br/>
+<br/>
+<table>
+  <tr>
+    <th>
+      clinic 2 :
+    </th>
+    <td>
+      356-c4c, `(gf)`
+    </td>
+    <td>
+      opp agarwaal sweets, janakpuri
+      new delhi -110058
+    </td>
+    </tr>
+    <tr>
+    <th>timing : </th>
+     <td> with appointment
+     </td>
+    </tr>
+</table>
+<h1 className="foot">DR. rk garg</h1>
+  </footer>
+  </div>
+   </div>
+   
+  );
+} 
+export default Leading;
